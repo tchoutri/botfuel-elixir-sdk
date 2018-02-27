9dd6d81477147a16008635865ecd418d659e9829
Théophile Choutri <theophile@choutri.eu>
2018-02-27 11:01:23 +0100

    Fix the argument passing

--- README.md
+++ README.md
@@ -1,21 +1,28 @@
 # Botfuel
 
-This is an unofficial Elixir SDK for the [Botfuel.io](https://app.botfuel.io/docs) NLP bot platform.
-
-This SDK is still under development and thus only available from github.  
-
+This is an (unofficial) Elixir SDK for the [Botfuel.io](https://app.botfuel.io/docs) NLP bot platform.
 Feedback and contributions are of course welcome.
 
 
-### Roadmap
+## Usage
+
+You can start the `Botfuel.Client` module manually in your Supervision tree, or use `Botfuel.new_client(%{app_id: <your app ID>, app_key: <your app key>})`.
+
+```Elixir
+iex(1)> Botfuel.new_client(%{app_id: app_id, app_key: app_key})
+{:ok, #PID<0.189.0>}
+```
+
+
+## Roadmap
 
 * [x] Basic support for [entity extraction](https://docs.botfuel.io/api#nlp-entity-extraction)
 * [x] Basic support for [spellchecking](https://docs.botfuel.io/api#nlp-spell-checking)
 * [x] Basic support for [QnA](https://docs.botfuel.io/api#qna)
 
-=> :tada: V0.1 :tada: <=
+:tada: V0.1 :tada:
 
-### Licence
+## Licence
 
 This software is licenced under the MIT licence.  
 See [LICENSE](LICENSE).  
--- lib/botfuel/request.ex
+++ lib/botfuel/request.ex
@@ -2,16 +2,19 @@ defmodule Botfuel.Request do
   require Logger
   use Tesla
 
-  plug Tesla.Middleware.Headers, %{"App-Id" => Application.get_env(:botfuel, :app_id),
+  plug Tesla.Middleware.Headers, %{"App-Id"  => Application.get_env(:botfuel, :app_id),
                                    "App-Key" => Application.get_env(:botfuel, :app_key)
                                   }
+
   plug Tesla.Middleware.JSON, engine: Jason
   plug Tesla.Middleware.BaseUrl, "https://api.botfuel.io"
-
+  plug Tesla.Middleware.DebugLogger
 
   def extract(params) do
     url = "/nlp/entity-extraction"
-    case get(url, query: Map.to_list(params)) do
+    params = params_to_list(params)
+    Logger.debug(inspect params)
+    case get(url, query: params) do
       %Tesla.Env{status: 200, body: body} -> {:ok, body}
       error ->
         Logger.error(inspect error)
@@ -38,4 +41,20 @@ defmodule Botfuel.Request do
         {:error, :error}
     end
   end
+
+  def params_to_list(params) when is_map(params) do
+    params
+    |> Map.delete(:__struct__)
+    |> Map.to_list
+    |> flatten_params([])
+  end
+
+  def flatten_params([_|[]], acc), do: acc
+  def flatten_params([{k,v}|t]=nested_list, acc) when is_list(v) do
+    Logger.debug("Blarb => " <> inspect nested_list)
+    flat = List.duplicate(k, length(v)) |> Enum.zip(v)
+    flatten_params(t, flat ++ acc)
+  end
+
+  def flatten_params([head|tail], acc), do: flatten_params(tail, [head] ++ acc)
 end
